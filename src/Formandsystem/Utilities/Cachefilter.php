5f3caadf4195920f5f9916b75719a9d4b7a8655b
Lukas Oppermann <lukas@vea.re>
2014-07-28 16:11:38 +0200

    update name

---
+++ src/Formandsystem/Utilities/Cachefilter.php
@@ -0,0 +1,27 @@
+<?php namespace Formandsystem\Utilities;
+/*
+ * utilities
+ *
+ * (c) Lukas Oppermann – vea.re
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @version
+ */
+
+use App;
+
+class Cachefilter {
+	/**
+	 * variable
+	 *
+	 * return variable or default
+	 *
+	 * @access	public
+	 */
+	public function fetch(  )
+	{
+		return 'yo man';
+	}
+}
